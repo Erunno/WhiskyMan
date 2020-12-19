fa812b3c685b7e52b88f166fa836182565a07e64
Vojtěch Mykiska <56797059@cuni.cz>
2020-12-19 02:07:52 +0100

    #10-Implementation of Registration form.

--- WhiskyMan.Angular/src/app/areas/user/register/register.component.ts
+++ WhiskyMan.Angular/src/app/areas/user/register/register.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {UserForRegistration} from '../x_models/user-for-registration';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../../environments/environment.prod';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {AuthService} from '../../x_shared/services/auth/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +11,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  user: UserForRegistration = { firstName: '', lastName: '',  username: '', password: '', email: ''};
+  baseAddr: string;
+  err: any;
+  form: any;
+  confirmedPasswd: string;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService
+  ) {
   }
 
+  ngOnInit(): void {
+    this.confirmedPasswd = '';
+  }
+
+  public register(): void{
+    this.authService.register(this.user, x => console.log(x), ex => console.log(ex));
+  }
 }
---
+++ WhiskyMan.Angular/src/app/areas/user/x_models/user-for-registration.ts
@@ -0,0 +1,7 @@
+export interface UserForRegistration {
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  email: string;
+}
