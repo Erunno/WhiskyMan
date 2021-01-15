ab3a361b5e73b8bbd006f913bc02da37857d66de
Matyáš Brabec <matyas.brabec@profinit.eu>
2021-01-15 12:48:19 +0100

    #35 - implemented backend of bottle list + maps tests

--- WhiskyMan.Backend/WhiskyMan.Repositories.Tests/Mapping/BottleMapsTest.cs
+++ WhiskyMan.Backend/WhiskyMan.Repositories.Tests/Mapping/BottleMapsTest.cs
@@ -26,6 +26,34 @@ namespace WhiskyMan.Repositories.Tests.Mapping
                 nameof(Bottle.Ownerships)
                 );
 
+        [Test]
+        public void Bottle_BottleView()
+        {
+            // arrange
+            var bottle = GetBottle();
+            var bottleView = GetBottleView();
+
+            // act + assert
+            var actual = TestMapping<Bottle, BottleView>(
+                input: bottle,
+                expected: bottleView,
+
+                // don't check following properties
+                nameof(BottleView.Owners),
+                nameof(BottleView.Tags)
+                );
+
+            // addintional asserts
+            CollectionAssert.AreEquivalent(
+                expected: bottleView.Tags,
+                actual: actual.Tags);
+
+            CollectionAssert.AreEquivalent(
+                expected: bottleView.Owners,
+                actual: actual.Owners);
+
+        }
+
 
         private Bottle GetBottle()
             => new Bottle
@@ -39,9 +67,22 @@ namespace WhiskyMan.Repositories.Tests.Mapping
                 WastePercent = 15,
                 Owners = new Collection<User>
                 {
-                    new User { Id = 1 },
-                    new User { Id = 2 },
-                    new User { Id = 3 }
+                    new User { Id = 1, FirstName = "F1", LastName = "L1" },
+                    new User { Id = 2, FirstName = "F2", LastName = "L2" },
+                    new User { Id = 3, FirstName = "F3", LastName = "L3" }
+                },
+                BottleDescription = new BottleDescription
+                {
+                    Id = 2,
+                    Name = "desc name",
+                    Distillery = "dist",
+                    Active = true,
+                    PictureUrl = "url",
+                    Region = "region",
+                    Age = 8,
+                    Tags = new List<Tag>() { new Tag { Id = 3, Name = "tag" } },
+                    DescriptionText = "desc text",
+                    Voltage = 40
                 }
             };
 
@@ -54,6 +95,21 @@ namespace WhiskyMan.Repositories.Tests.Mapping
                 OwnerIds = new List<int> { 1, 2, 3 },
                 ShotPrice = 20,
                 WastePercent = 15
+            };
+
+        private BottleView GetBottleView()
+            => new BottleView
+            {
+                Id = 1,
+                Name = "desc name",
+                Distillery = "dist",
+                PictureUrl = "url",
+                Owners = new List<string>()
+                {
+                    "F1 L1", "F2 L2", "F3 L3"
+                },
+                Tags = new List<string>() { "tag" },
+                ShotPrice = 20
             };
     }
 }
